2559ef55189cf30d923ba3c3f1b56550219548ac
ahmedDev32 <166875212+ahmedDev32@users.noreply.github.com>
2024-07-13 12:13:46 +0500

    Update Products.jsx

--- pages/Products.jsx
+++ pages/Products.jsx
@@ -104,7 +104,7 @@ let [Down,SetDown] = useState(false)
 
      </div>
      <button
-  className="fixed left-4 bg-gray-300 text-gray-800 py-2 px-4 rounded-lg shadow-md sm:py-3 sm:px-6 sm:rounded-md flex items-center justify-center"
+  className="bg-gray-300 text-gray-800 py-2 px-4 rounded-lg  m-autoshadow-md sm:py-3 sm:px-6 sm:rounded-md flex items-center justify-center"
   onClick={() => window.history.back()}
 >
   <FaArrowLeft className="mr-2" /> Go Back
